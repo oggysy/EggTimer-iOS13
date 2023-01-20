5d296f0e3f4bded40e1d52a8b281d3bf0806b9d5
oggysy <ogisoys@gmail.com>
2023-01-20 21:59:43 +0900

    メソッドの中の条件式をguardに変更

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -23,21 +23,18 @@ class ViewController: UIViewController {
         guard let hardness = sender.currentTitle, let totalTime = eggTimes[hardness] else { return }
         titleLavel.text = hardness
         timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { (timer) in
-            if secondsPassed < totalTime {
-                    secondsPassed += 1
-                let percentageProgress = Float(secondsPassed) / Float(totalTime)
-                self.progressBar.progress = percentageProgress
-                self.timeLeft.text = String(totalTime - secondsPassed)
-                } else {
-                    timer.invalidate()
-                    self.titleLavel.text = "Done!"
-                    self.playSound()
-                }
+            guard secondsPassed < totalTime else {
+                timer.invalidate()
+                self.titleLavel.text = "Done!"
+                self.playSound()
+                return
             }
-        
-        
+            secondsPassed += 1
+            let percentageProgress = Float(secondsPassed) / Float(totalTime)
+            self.progressBar.progress = percentageProgress
+            self.timeLeft.text = String(totalTime - secondsPassed)
+        }
     }
-    
     func playSound(){
         let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
         player = try! AVAudioPlayer(contentsOf: url!)
